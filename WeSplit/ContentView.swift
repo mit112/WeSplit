c5c1f168a968b59258a99ec93965101ab9f958bb
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2024-01-11 11:19:00 -0500

    Commit 2

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Mit Sheth on 1/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            ///commit2
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
